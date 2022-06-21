82e41d8a540849546c2d7fcbf64d32502325beee
Angel Juarez <angeljuarez729@gmail.com>
2022-06-21 03:23:22 -0400

    action field

--- server/src/routes/ifttt.js
+++ server/src/routes/ifttt.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/v1/actions/reddit_saved_post', async (req, res) => {
+router.post('/v1/actions/reddit_saved_post/fields/title/options', async (req, res) => {
   try {
     console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
     console.log(req);
