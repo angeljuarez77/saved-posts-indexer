e673b12e4d9ae8dd4e490969973ea5c48b13490c
Angel Juarez <angeljuarez729@gmail.com>
2022-06-21 01:59:51 -0400

    send body

--- server/src/routes/ifttt.js
+++ server/src/routes/ifttt.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post('/v1/actions/reddit_saved_post', async (req, res) => {
   try {
-    console.log(req.body);
+    res.json(req.body);
   } catch(e) {
     console.error(e.message);
   }
